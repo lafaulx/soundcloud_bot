143473ee545f7b4b4e48f926efc892abde86fc0d
Yaroslav Kravchenko <jsslzrd@gmail.com>
2016-06-15 22:18:15 +0200

    Added telegram and soundcloud db operations

---
+++ dbCreate.js
@@ -0,0 +1,41 @@
+const MongoClient = require('mongodb').MongoClient;
+const bunyan = require('bunyan');
+const async = require('async');
+
+const config = require('./local_config');
+const DB_COLLECTIONS = require('./lib/consts/dbCollections');
+
+const MONGODB_ADDR = config.MONGODB_ADDR;
+const MONGODB_PORT = config.MONGODB_PORT;
+const MONGODB_DB = config.MONGODB_DB;
+
+const mongodbUrl = `mongodb://${MONGODB_ADDR}:${MONGODB_PORT}/${MONGODB_DB}`;
+
+const log = bunyan.createLogger({
+  name: 'sndcld_bot_create_db'
+});
+
+MongoClient.connect(mongodbUrl, function(err, db) {
+  log.info('Connected to MongoDB');
+
+  async.series([
+    function(cb) {
+      db.collection(DB_COLLECTIONS.TELEGRAM_USERS).ensureIndex('id', {
+        unique: true
+      }, cb);
+    },
+    function(cb) {
+      db.collection(DB_COLLECTIONS.SOUNDCLOUD_USERS).ensureIndex('id', {
+        unique: true
+      }, cb);
+    }
+  ], function(err, results) {
+    if (err) {
+      log.error(err);
+    } else {
+      log.info('DB tasks successfully completed: ', results);
+    }
+
+    db.close();
+  });
+});
