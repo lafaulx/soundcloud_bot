8e2aa3c4c5618b49ebb4998aa2ee58439c15663d
Yaroslav Kravchenko <jsslzrd@gmail.com>
2016-06-27 14:01:41 +0200

    Added cron

---
+++ cron.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const MongoClient = require('mongodb').MongoClient;
+const CronJob = require('cron').CronJob;
+const bunyan = require('bunyan');
+const telegramNodeBot = require('telegram-node-bot');
+
+const config = require('./local_config');
+const scFn = require('./lib/sc');
+const dbFn = require('./lib/db');
+const opsFn = require('./lib/ops');
+
+const MONGODB_ADDR = config.MONGODB_ADDR;
+const MONGODB_PORT = config.MONGODB_PORT;
+const MONGODB_DB = config.MONGODB_DB;
+const TELEGRAM_AUTH_TOKEN = config.TELEGRAM_AUTH_TOKEN;
+const SOUNDCLOUD_CLIENT_ID = config.SOUNDCLOUD_CLIENT_ID;
+
+const sc = scFn(SOUNDCLOUD_CLIENT_ID);
+const tg = telegramNodeBot(TELEGRAM_AUTH_TOKEN);
+
+const mongodbUrl = `mongodb://${MONGODB_ADDR}:${MONGODB_PORT}/${MONGODB_DB}`;
+
+const log = bunyan.createLogger({
+  name: 'sndcld_bot_create_db'
+});
+
+new CronJob({
+  cronTime: '00 00 */2 * * *',
+  onTick: function() {
+    MongoClient.connect(mongodbUrl, function(err, dbConn) {
+      log.info('Connected to MongoDB');
+
+      const db = dbFn(dbConn);
+      const ops = opsFn(sc, db, tg);
+
+      ops.updateTracks().then(() => dbConn.close());
+    });
+  },
+  start: true
+});
--- dbCreate.js
+++ dbCreate.js
@@ -1,7 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const bunyan = require('bunyan');
 const async = require('async');
-const _ = require('lodash');
 
 const config = require('./local_config');
 const DB_COLLECTIONS = require('./lib/consts/dbCollections');
